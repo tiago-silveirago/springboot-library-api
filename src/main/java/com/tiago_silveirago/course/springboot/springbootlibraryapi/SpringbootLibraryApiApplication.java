4291e2d8d08b95ef30b3aa07d067dba576205d80
Tiago Silveira <tiago.silveirago@gmail.com>
2025-08-06 15:23:01 -0300

    feat(core): set up initial Spring Boot project with Maven and essential dependencies

---
+++ src/main/java/com/tiago_silveirago/course/springboot/springbootlibraryapi/SpringbootLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.course.springboot.springbootlibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiago_silveirago/course/springboot/springbootlibraryapi/SpringbootLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.course.springboot.springbootlibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
