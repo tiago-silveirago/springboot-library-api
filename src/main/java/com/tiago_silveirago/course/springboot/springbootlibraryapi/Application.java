88e23b80a2123ddef24007c2ff72507d0116e83c
Tiago Silveira <tiago.silveirago@gmail.com>
2025-08-11 18:05:13 -0300

    feat(config): configure datasource and connection pool using HikariCP

--- src/main/java/com/tiago_silveirago/course/springboot/springbootlibraryapi/Application.java
+++ src/main/java/com/tiago_silveirago/course/springboot/springbootlibraryapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootLibraryApiApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootLibraryApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
